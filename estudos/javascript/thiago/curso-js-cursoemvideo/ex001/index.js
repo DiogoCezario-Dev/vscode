32e695faee13898b9fcf553bdd4186f28bf71ea7
DiogoCezario-Dev <152320636+DiogoCezario-Dev@users.noreply.github.com>
2023-11-29 14:19:24 -0300

    add atv 001 cursoemvideo

---
+++ estudos/javascript/thiago/curso-js-cursoemvideo/ex001/index.js
@@ -0,0 +1,4 @@
+window.alert("Aqui vai um alerta usando o window.alert")
+window.confirm("Essa qui é uma confirmação usando o window.confirm")
+var nome = window.prompt("Aqui está um exemplo do window.prompt já colocando em uma variavel tudo que você escrever aqui")
+window.alert("Isso foi oque você escreveu " + nome)
